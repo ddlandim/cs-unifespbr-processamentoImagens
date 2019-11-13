49e1ff72f4e5d225a2e99372a49b744aa51c0b51
ddlandim <mrdfane@gmail.com>
2019-11-13 12:07:38 -0300

    rgb - hsi
    corrigindo conversao errada em H para complexo

--- imagens_coloridas.m
+++ imagens_coloridas.m
@@ -8,94 +8,66 @@ R = img(:,:,1);
 G = img(:,:,2);
 B = img(:,:,3);
 
-img_hsi = img;
-
 %I = 1/3*(R+G+B)
-img_hsi(:,:,3) = (R+G+B)/3;
-I = img_hsi(:,:,3);
-
+I = (R+G+B)/3;
 for x = 1:l 
     for y = 1:c
         r = R(x,y);
         g = G(x,y);
         b = B(x,y);
-        %S = 1-(min(R,G,B)/I)
-        if (I(x,y)==0) img_hsi(x,y,2) = 1;
-            else  img_hsi(x,y,2) = 1 - ( min( min(r,g),b) ) / I(x,y) ;
+
+        %--S = 1-(min(R,G,B)/I)
+        if (I(x,y)==0) S(x,y) = 1;
+            else  S(x,y) = 1 - ( min( min(r,g),b) ) / I(x,y) ;
         end
         
-        %H
-        if (img_hsi(x,y,2) == 0) img_hsi(x,y,1) = 0;
+        %--H
+        %---indefinido se S=0
+        if (S(x,y) == 0) H(x,y) = 0;
+        
+        %--theta
         else
-                aux = ( (r-g)^2 + ( (r-g)*(g-b) ))^0.5;
-                if(aux~=0)
-                    theta = acos( (0.5*( (r-g)+(r-b))) / aux);
+                %denominador aux
+                aux1 = (0.5*((r-g)+(r-b)))
+                aux2 = sqrt( (r-g)^2 + ((r-g)*(g-b)) );
+                
+                if(aux2~=0)
+                    aux3 = aux1/aux2;
+                    theta = acosd(aux3);
                 end
-                theta = pi/2;
-                if(b<=g) img_hsi(x,y,1) = theta;
-                    else img_hsi(x,y,1) = (2*pi)-theta;
+                
+                %theta calculado
+
+                %Calculando H
+                if(b<=g) H(x,y) = theta;
+                    else H(x,y) = 360-theta;
                 end
         end
     end
 end
 
-%H/360
-img_hsi(:,:,1) = img_hsi(:,:,1)/(2*pi);
+%H/360, convertendo H para o dominio de 0 a 1
+H = H/360;
+
+% H, S, I finalizados.
 
 figure;
-imagesc(img_hsi(:,:,1));
-
-colormap(gray);title('H');
-
-figure;
-imagesc(img_hsi(:,:,2));
-
-colormap(gray); title('S');
+imagesc(H);
+colormap(gray);
+title('H');
 
 figure;
-imagesc(img_hsi(:,:,3)); 
+imagesc(S);
+colormap(gray); 
+title('S');
+
+figure;
+imagesc(I); 
 colormap(gray);
 title('I');
 
 %INVERSA HSI PARA RGB
-img_rgb = img_hsi;
-%Convertendo H de Rad para Graus
-img_rgb(:,:,3) = img_rgb(:,:,3)*180;
-
-H = img_hsi(:,:,1);
-S = img_hsi(:,:,2);
-I = img_hsi(:,:,3);
+%Convertendo H para o domínio de 0 a 360.
+H_rgb = H*180;
 
 %Setor RG: se 0 <= H <= 120
-for x = 1:l 
-    for y = 1:c
-        
-        h = H(x,y);
-        s = S(x,y);
-        i = I(x,y);
-        
-        r = 
-        %S = 1-(min(R,G,B)/I)
-        if (I(x,y)==0) img_hsi(x,y,2) = 1;
-            else  img_hsi(x,y,2) = 1 - ( min( min(r,g),b) ) / I(x,y) ;
-        end
-        
-        %H
-        if (img_hsi(x,y,2) == 0) img_hsi(x,y,1) = 0;
-        else
-                aux = ( (r-g)^2 + ( (r-g)*(g-b) ))^0.5;
-                if(aux~=0)
-                    theta = acos( (0.5*( (r-g)+(r-b))) / aux);
-                end
-                theta = pi/2;
-                if(b<=g) img_hsi(x,y,1) = theta;
-                    else img_hsi(x,y,1) = (2*pi)-theta;
-                end
-        end
-    end
-end
-
-
-
-
-
